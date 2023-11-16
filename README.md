6eec90358b5129149e96d17ff79db9e977a906c1
Salman M. <salman-m@web.de>
2023-11-16 10:00:12 +0100

    Update the Script, Add Readme file

---
+++ README.md
@@ -0,0 +1,35 @@
+# Testing Node.js Server with Postman
+
+1. **Open Postman.**
+
+2. **Create a new request:**
+   - Set the request type to **POST.**
+   - Enter the URL: `http://localhost:<PORT>/run.`
+
+3. **In the Headers tab, add a new header:**
+   - Key: `Content-Type`
+   - Value: `application/json`
+
+4. **Switch to the Body tab, select the raw option, and enter the JSON data for your request.**
+
+   - For running a script:
+
+     ```json
+     {
+       "path": "your_script_path.ps1"
+     }
+     ```
+
+   - For running a command:
+
+     ```json
+     {
+       "command": "Get-Process"
+     }
+     ```
+
+   Make sure to replace `"your_script_path.ps1"` with the actual path to your PowerShell script.
+
+5. **Click the "Send" button to make the request.**
+
+6. **Check the response in the "Body" section of the response to see the output of your PowerShell script or command.**
--- index.js
+++ index.js
@@ -1,27 +1,49 @@
 import express from "express";
 import { PowerShell } from "node-powershell";
-PowerShell.$`echo "hello from PowerShell"`;
+
 const ps = new PowerShell({
   executionPolicy: "Bypass",
   noProfile: true,
 });
-const app = express();
-const port = 3000;
 
-app.get("/:path", function (req, res) {
-  console.log(req.params.path);
-  ps.invoke(` ${req.params.path}`)
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+app.post("/run", (req, res) => {
+  const { command, path } = req.body;
+
+  if (!command && !path) {
+    return res.status(400).json({ error: "Command or path is required" });
+  }
+
+  let script = command || path;
+
+  console.log(`Executing PowerShell ${command ? 'command' : 'script'}: ${script}`);
+
+  //ps.addCommand();
+
+  ps.invoke(script)
     .then((response) => {
+      console.log("Execution successful");
       res.send(response);
     })
     .catch((err) => {
-      res.json(err);
+      console.error("Error executing PowerShell script:", err);
+      res.status(500).json({ error: "Internal server error" });
     });
 });
 
-app.post("/login", function (req, res) {
-  res.send("welcome");
+app.get("/", (req, res) => {
+  res.send("Welcome to the PowerShell runner!");
 });
+
 app.listen(port, () => {
-  return `nodejs is running on Port ${port}`;
+  console.log(`Node.js server is running on Port http://localhost:${port}`);
+});
+
+// Handle graceful shutdown
+process.on("SIGINT", () => {
+  console.log("\nShutting down gracefully");
+  ps.dispose().then(() => process.exit());
 });
